6167f63397495ec6e7c89b65148ee6402461676d
Vincent Riva <vincent.riva@outlook.fr>
2023-09-21 18:10:11 +0200

    🔧 Change bundling configuration

--- site/templates/default.blade.php
+++ site/templates/default.blade.php
@@ -1,14 +1,17 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Document</title>
 </head>
 
 <body>
   Hello World !!
+  {{ site()->title }}
+  <span class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Test</span>
+  <span class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Test</span>
 </body>
 
 </html>

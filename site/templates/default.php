82c0a203af0a06766fa6b623b6a474dde078543f
Vincent Riva <vincent.riva@outlook.fr>
2023-09-21 07:22:42 +0200

    🎨 Begin to add base files

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,17 @@
-<h1><?= $page->title() ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?= vite()->css('assets/js/app.css') ?>
+</head>
+
+<body>
+    <h1 class="text-red-500 text-6xl"><?= $page->title() ?> World</h1>
+
+    <?= vite()->js('assets/js/app.js') ?>
+</body>
+
+</html>

a09573f58d6da4beac06a0e4967e5eaf258ed5d1
Vincent Riva <vincent.riva@outlook.fr>
2023-09-23 15:22:33 +0200

    ➕ Add Kirby plugins (SEO Meta & Navigation)

--- site/templates/default.blade.php
+++ site/templates/default.blade.php
@@ -4,12 +4,13 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico">
   <title>Document</title>
 </head>
 
 <body>
   Hello World !!
-  {{ site()->title }}
+  {{ $page->title() }}
   <span class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Test</span>
   <span class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Test</span>
 </body>

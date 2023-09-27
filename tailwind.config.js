fe1441f0d760490130142490f598fa08f4322c75
Vincent Riva <vincent.riva@outlook.fr>
2023-09-27 11:13:29 +0200

    💄 Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@
 module.exports = {
   content: [
     "./index.html",
-    "./assets/**/*.{js}",
-    "./site/**/*.{php,js,ts,jsx,tsx}",
+    "./assets/**/*.{js,scss}",
+    "./site/**/*.{php,js,ts,jsx,tsx,blade.php}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
